f477c93d7073a95f4f5c894573872b2c307182f3
Stefan Dörig <sdoerig@bluewin.ch>
2020-06-12 18:11:02 +0200

    Some more testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,21 @@
-
 use pyo3::prelude::*;
-use pyo3::wrap_pyfunction;
 
 
 fn is_prime(num: u64) -> bool {
-    if num <= 1 {
+    // Checks it a positive integer is prime and returns true
+    // if it is. 
+    // The algorithm is little optimized and just inteded for demonstrations.
+    if num <= 1 || num == 4 {
         false
+    } else if num == 2 || num == 3 {
+        true
     } else {
         let mut res = true;
-        for i in 2..num {
-            if num % i == 0 {
+        let partial_num_range = num / 4 + 1;
+        
+        for i in 1..partial_num_range {
+            let divisor = 2 * i + 1;
+            if num % divisor == 0 {
                 res = false;
                 break;
             }
@@ -20,9 +26,11 @@ fn is_prime(num: u64) -> bool {
 
 
 
-/// This module is implemented in Rust.
+/// py_prime_checker implemented in rust
+/// Functions:
+/// - is_prime 
 #[pymodule]
-fn rust2py(py: Python, m: &PyModule) -> PyResult<()> {
+fn py_prime_checker(_py: Python, m: &PyModule) -> PyResult<()> {
     // PyO3 aware function. All of our Python interfaces could be declared in a separate module.
     // Note that the `#[pyfn()]` annotation automatically converts the arguments from
     // Python objects to Rust values, and the Rust return value back into a Python object.
@@ -40,9 +48,40 @@ fn rust2py(py: Python, m: &PyModule) -> PyResult<()> {
 
 #[cfg(test)]
 mod tests {
+    // For primes see https://bigprimes.org - thanks, great job. 
     use super::is_prime;
     #[test]
-    fn it_works() {
+    fn test_small_primes() {
+        assert_eq!(is_prime(2), true);
+        assert_eq!(is_prime(3), true);
         assert_eq!(is_prime(5), true);
+        assert_eq!(is_prime(11), true);
+        assert_eq!(is_prime(3), true);
     }
+    #[test]
+    fn test_small_non_primes() {
+        assert_eq!(is_prime(4), false);
+        assert_eq!(is_prime(49), false);
+        assert_eq!(is_prime(21), false);
+    }
+
+    #[test]
+    fn test_medium_primes() {
+        assert_eq!(is_prime(839323), true);
+        assert_eq!(is_prime(179689), true);
+        assert_eq!(is_prime(413081), true);
+        assert_eq!(is_prime(799801), true);
+    }
+
+    #[test]
+    fn test_medium_non_primes() {
+        // combined by two primes multiplied - will be crunced at 
+        // the smallest prime factor.
+        assert_eq!(is_prime(317701 * 895313), false);
+        assert_eq!(is_prime(658979 * 852457), false);
+        assert_eq!(is_prime(200273 * 877469), false);
+        assert_eq!(is_prime(738851 * 384547), false);
+    }
+
+    
 }
