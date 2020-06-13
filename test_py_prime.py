ab5e2ad9ae61fb4183a5d0d84880aa350d049c01
Stefan Dörig <sdoerig@bluewin.ch>
2020-06-13 21:33:54 +0200

    Swapped rust with git test

--- test_py_prime.py
+++ test_py_prime.py
@@ -22,17 +22,17 @@ class TestPrimeImplementations(unittest.TestCase):
         self.assertEqual(PyPrime.is_prime_by_python(21), False)
     
     def test_prime_crunching_time(self):
-        py_start_time = timer()
-        py_is_prime = PyPrime.is_prime_by_python(TestPrimeImplementations.primes[0])
-        py_time_elapesed = timer() - py_start_time
-        print("Python implementation: {} is prime {} time elapsed {}"
-        .format( TestPrimeImplementations.primes[0], py_is_prime, py_time_elapesed))
-
         rs_start_time = timer()
         rs_is_prime = PyPrime.is_prime_by_rust(TestPrimeImplementations.primes[0])
         rs_time_elapesed = timer() - rs_start_time
         print("Rust implementation: {} is prime {} time elapsed {}"
         .format( TestPrimeImplementations.primes[0], rs_is_prime, rs_time_elapesed))
+
+        py_start_time = timer()
+        py_is_prime = PyPrime.is_prime_by_python(TestPrimeImplementations.primes[0])
+        py_time_elapesed = timer() - py_start_time
+        print("Python implementation: {} is prime {} time elapsed {}"
+        .format( TestPrimeImplementations.primes[0], py_is_prime, py_time_elapesed))
 
         print("Rust is {} times faster".format(py_time_elapesed / rs_time_elapesed))
 
